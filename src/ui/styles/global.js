a17d9c4be76dffebdaeb619a22dae247000b95f2
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-19 17:07:56 -0300

    adding folders

---
+++ src/ui/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+import { theme } from './theme'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${theme};
+    font-family: ${theme};
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
