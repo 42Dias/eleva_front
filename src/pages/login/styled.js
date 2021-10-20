5dbbf5496b94fd21263679886803ce021e7dcdeb
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-20 09:36:54 -0300

    adding theme and chakra UI in the project

---
+++ src/pages/login/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { theme } from '../../ui/styles/theme'
+
+export const Teste = styled.div`
+  background: ${theme.colors.green};
+`
--- src/ui/styles/theme.js
+++ src/ui/styles/theme.js
@@ -1,1 +1,21 @@
-export const theme = {}
+export const theme = {
+  colors: {
+    white: '#FFFF',
+    blue: '#050C21',
+    green: '#05DEBD',
+    red: '#AA2323',
+    neutral: {
+      100: '#F2F2F2',
+      200: '#F5F5F5',
+      300: '#F9F9F9',
+      400: '#BFBFBF',
+    },
+  },
+  font: {
+    poppins: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+}
