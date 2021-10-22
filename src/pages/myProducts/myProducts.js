4f9170d6d825a8bec60d80e6ac6c6be24afb5982
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2021-10-22 09:49:23 -0300

    components header

--- src/pages/myProducts/myProducts.js
+++ src/pages/myProducts/myProducts.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { LOGIN } from '../../routes'
+import Header from './../../components/Header'
 
 export function MyProducts() {
-  return (
-    <>
-      <h1>Meus Produtos</h1>
-      <Link to={LOGIN}>Navegar</Link>
-    </>
-  )
+  return <Header />
 }
