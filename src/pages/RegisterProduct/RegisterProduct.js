831b2b47f2b6a4ac45eff809217790b00712afb6
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-26 11:48:02 -0300

    adding register product

--- src/pages/RegisterProduct/RegisterProduct.js
+++ src/pages/RegisterProduct/RegisterProduct.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import Navbar from '../../components/Sidebar/Sidebar'
 // import Footer from './../../components/Footer'
+import IMAGE from '../../assets/upload.png'
 import * as S from './styled'
 
 export function RegisterProduct() {
   return (
     <S.ContainerLogin>
       <Navbar />
+      <h1>Novo produto</h1>
+      <S.BoxImage>
+        <img src={IMAGE} alt='upload image' />
+      </S.BoxImage>
       <S.Content>
-        <h1>Novo produto</h1>
+        <form className='form'>
+          <label htmlFor='name'>Nome do produto</label>
+          <S.Input id='name' type='text' placeholder='Nome do produto' />
+          <label htmlFor='code'>Código</label>
+          <S.Input id='code' type='text' placeholder='Código do produto' />
+          <label htmlFor='description'>Descrição</label>
+          <S.Input
+            id='description'
+            type='text'
+            placeholder='Descrição do produto'
+          />
+          <label htmlFor='ref'>Referência técnica</label>
+          <S.Input id='ref' type='text' placeholder='Referência do produto' />
+          <label>Rede SKU</label>
+          <S.Input id='yes' type='radio' />
+          <label htmlFor='yes'>Sim</label>
+          <S.Input id='no' type='radio' />
+          <label htmlFor='no'>Não</label>
+        </form>
       </S.Content>
     </S.ContainerLogin>
   )
--- src/pages/RegisterProduct/styled.js
+++ src/pages/RegisterProduct/styled.js
@@ -4,11 +4,37 @@ import { theme } from '../../ui/styles/theme'
 export const ContainerLogin = styled.div`
   width: 100vw;
   height: 100vh;
+
+  h1 {
+    margin: 10px;
+    color: ${theme.colors.blue};
+  }
 `
 
 export const Content = styled.div`
-  color: ${theme.colors.blue};
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+export const BoxImage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Input = styled.input`
+  width: 200px;
+  height: 48px;
+  background: ${theme.colors.white};
+  border-radius: 4px;
+  border: none;
+  color: ${theme.colors.blue};
+  padding: 0 0 0 10px;
+  margin: 0 0 20px 0;
+
+  &[type='radio'] {
+    height: 15px;
+    width: 15px;
+  }
 `
