1e978f49431c3640daacec11a85a4638c86624be
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-26 17:01:14 -0300

    refatorado com sucesso!

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
+
 import LOGO from '../../assets/logo.png'
-import { apiWithoutTenant, loadingGif, porta, token,  id, ip} from '../../services/api'
+import { apiWithoutTenant, loadingGif, porta, token,  id, ip, apiWithoutTenantAndWithToken} from '../../services/api'
+
+import loadUser from '../../services/loadUser' 
+
 import * as S from './styled'
 import InputMask from 'react-input-mask';
 import axios from 'axios'
+
+
+
+import cadastro from '../../services/cadastro'
 
 import { toast } from 'react-toastify';
 
@@ -18,124 +26,30 @@ export  default function Register() {
   const [phoneMaskedNumber, setphoneMaskedNumber] = useState('')
 
   const [loading, setLoading] = useState(false);
-  function handleLocalStorage(emailA, passwordB) {
+  
+  let history = useHistory();
 
-    localStorage.setItem("email", JSON.stringify(emailA));//saves client's data into localStorage:
-    localStorage.setItem("password", JSON.stringify(passwordB));//saves client's data into localStorage:
-  }
-  async function sendEmail() {
-    // Axios.post(`${ip}:8157/api/cliente/${id}/${token}/verificarEmail`, {
-    //   email: email
-    // })
-    apiWithoutTenant.post(`cliente/${id}/${token}/verificarEmail`)
-    .then((response) => {
-      if (response.statusText == "OK") {
-        toast.info('Email enviado com sucesso!');
-       
-        setLoading(false)
-        handleClickLogin();
-      } 
-      else {
-        toast.error('Email não enviado com sucesso!');
-      }
-    });
-  }
-
-
-  console.log(token)
-
-
-  async function loadUser(token) {
-    const response = await axios({
-      method: 'get',
-      url: `${ip}:8144/api/auth/me`,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      },
-      timeout: 50000
-    }).then(response => {
-      return response.data;
-    })
-    // let response = await apiWithoutTenant.get('auth/me').then(response => {
-    //      return response.data;
-    //   })
-    console.log(response);
-    // let setRole = response.tenants[0].roles
-    // const roleHelper = JSON.parse(setRole)
-    // console.log(roleHelper[0])
-    // localStorage.setItem("roles", JSON.stringify(roleHelper[0])); //saves client's data into localStorage:
-    localStorage.setItem("roles", response.tenants[0].roles[0]); //saves client's data into localStorage:
-
-    console.log(response.tenants[0].tenant.id);
-    localStorage.setItem("tenantId", JSON.stringify(response.tenants[0].tenant.id))
-    localStorage.setItem("id", JSON.stringify(response.id))
-    localStorage.setItem("status", JSON.stringify(response.tenants[0].status))
-    // sendEmail();
-  }
-  function handleLocalStorageToken(token) {
-    const setLocalStorage = (data) => {
-      localStorage.setItem("token", JSON.stringify(data)); //saves client's data into localStorage:
-    };
-    setLocalStorage(token);
-    loadUser(token)
+  function goHome() {
+    history.push("/home");
   }
 
   function handleCreateUser(e) {
     e.preventDefault()
 
     toast.info('Carregando')
-
-    const data = {
-      nome,
-      email,
-      senha,
-      role: parseInt(category),
-      status: ''
-      // category,
-    }
-    console.log(data)
-    Cadastro()
-  }
-  async function Cadastro() {
     setLoading(true)
-    apiWithoutTenant.post('auth/sign-up', {
-        fullName: nome,
-        email: email,
-        password: senha,
-        role: parseInt(category),
-        status: ''
-      })
-      .then((response) => {
-        //first check the http response, returning the result to user
-        console.log(response)
-        if (response.statusText === 'OK') {
-          toast.info('Opa, recebemos o seu registro :)')
-          console.log("ok")
-          handleLocalStorage(email, senha)
-          handleLocalStorageToken(response.data)
-        } 
-        else if (response.statusText === 'Forbidden') {
-          console.log("Forbidden")
 
-          toast.error('Ops, Não tem permisão!')
-          setLoading(false)
-        }
-        else {
-          toast.error('Ops, Dados Incorretos!')
-          console.log("?")
-          
-          setLoading(false)
-
-        }
-      })
-      .catch((res) => {
-          console.log("catch")
-          console.log(res)
-        toast.error('ERROR')
+    // email senha role e status
+    cadastro(nome, email, senha, parseInt(category), '')
+    .then(
+      (cadastroStatus) => {
         setLoading(false)
-      })
+        cadastroStatus == 'ok' ? goHome() : toast.error("Algo deu errado :(")
+      }
+    )
+    .catch(
+      () => setLoading(false)
+    )
   }
 
   return (
@@ -234,3 +148,20 @@ export  default function Register() {
     </>
   )
 }
+// async function sendEmail() {
+//   // Axios.post(`${ip}:8157/api/cliente/${id}/${token}/verificarEmail`, {
+//   //   email: email
+//   // })
+//   apiWithoutTenant.post(`cliente/${id}/${token}/verificarEmail`)
+//   .then((response) => {
+//     if (response.statusText == "OK") {
+//       toast.info('Email enviado com sucesso!');
+     
+//       setLoading(false)
+//       handleClickLogin();
+//     } 
+//     else {
+//       toast.error('Email não enviado com sucesso!');
+//     }
+//   });
+// }
