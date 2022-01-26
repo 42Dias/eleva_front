8c684e4fe4e01543544f8ead4d03f05533b20200
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-26 16:58:19 -0300

    adicionado a informação do cadastro

---
+++ src/services/cadastro.js
@@ -0,0 +1,34 @@
+/*
+Função usada no cadastro
+*/
+
+import {apiWithoutTenant} from './api'
+import handleLocalStorageEmailAndPassword from '../utils/handleLocalStorageEmailAndPassword'
+import responseHandler from '../utils/responseHandler'
+import loadUser from './loadUser'
+import servidorErrorMessage from '../utils/servidorErrorMessage'
+
+
+export default async function cadastro(nome, email, senha, categoria, status) {
+    return apiWithoutTenant.post('auth/sign-up', {
+        fullName: nome,
+        email: email,
+        password: senha,
+        role: categoria,
+        status: status
+      })
+      .then((response) => {
+        let mensagemOk = 'Opa, recebemos o seu registro :)'
+        responseHandler(response.status, mensagemOk)
+        if (response.statusText === 'OK') {
+          //first check the http response, returning the result to user
+          handleLocalStorageEmailAndPassword(email, senha)
+          loadUser(response.data)
+          return 'ok'
+        }
+
+      })
+      .catch(() => {
+        servidorErrorMessage()
+      })
+  }
