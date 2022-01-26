332ee82ea21616ac832c4d0c24b606342b285210
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-26 13:49:51 -0300

    adicionado arquivo api

---
+++ src/services/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export let token = localStorage.getItem("token")?.replace(/"/g, "");
+export let tenantId = localStorage.getItem("tenantId")?.replace(/"/g, "");
+export let role = localStorage.getItem("roles")?.replace(/"/g, "");
+export let id = localStorage.getItem("id")?.replace(/"/g, "");
+export let status = localStorage.getItem("status")?.replace(/"/g, "");
+export let Email = localStorage.getItem("email")?.replace(/"/g, "");
+export const semImagem = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Item_sem_imagem.svg/1024px-Item_sem_imagem.svg.png"
+export const loadingGif = 'https://contribua.org/mb-static/images/loading.gif'
+
+
+export let ip = "http://localhost";//teste local
+// export let ip = "189.127.14.11";//servidor
+
+export let porta = '8144'
+
+export const api = axios.create({
+  baseURL: `${ip}:${porta}/api/tenant/${tenantId}/`,
+  // baseURL: 'http://'+ip+':8157/api/tenant/'+tenantId || "fa22705e-cf27-41d0-bebf-9a6ab52948c4" +"/",
+  timeout: 50000,
+  headers: {'Authorization': 'Bearer '+ token}
+});
+
+export const apiWithoutTenant = axios.create({
+  baseURL: `${ip}:${porta}/api/`,
+  // baseURL: 'http://'+ip+':8157/api/tenant/'+tenantId || "fa22705e-cf27-41d0-bebf-9a6ab52948c4" +"/",
+  timeout: 50000,
+  // headers: {'Authorization': 'Bearer '+ token}
+});
+
