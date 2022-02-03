cb120d5fa3e17cc5449634fda8c401ad2c067b7a
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-03 11:30:54 -0300

    comentado o log

--- src/services/loadEmpresaQuantidade.js
+++ src/services/loadEmpresaQuantidade.js
@@ -5,7 +5,7 @@ import { api } from "./api";
 export default async function loadEmpresaQuantidade(){
     return await api.get(`empresa-count`)
           .then((response) => {
-            console.log(response)
+            // console.log(response)
             let data = response.data[0]
             return data.quantidadeDeEmpresas 
           });
