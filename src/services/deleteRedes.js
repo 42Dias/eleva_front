fec4e178ea52f88090f2d23c5c62e93430ca59f4
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-04 10:42:17 -0300

    adicionada uma função para deletar  uma rede

---
+++ src/services/deleteRedes.js
@@ -0,0 +1,19 @@
+import responseHandler from "../utils/responseHandler";
+import { api } from "./api";
+
+
+export default async function deleteRedes(id){
+    // return await api.get(`user?filter%5B${filter}%5D=${valor}`).then(
+    return await api.delete(`rede/${id}`).then(
+    // await api.get(`empresa?`).then(
+        (res) => {
+            let status = res.status 
+            let mensagemOk = 'Rede deletada com sucesso!'
+            let mensagemNaoOK = 'Algo deu errado :('
+
+            responseHandler(status, mensagemOk, mensagemNaoOK)
+            console.log(res)
+        }
+    
+    )
+}
