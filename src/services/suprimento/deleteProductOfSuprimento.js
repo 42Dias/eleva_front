afa48c341d8dec83cdc398ab8b5a427b79d510c9
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-15 09:36:42 -0300

    modificada a rota

--- src/services/suprimento/deleteProductOfSuprimento.js
+++ src/services/suprimento/deleteProductOfSuprimento.js
@@ -3,7 +3,7 @@ import { api } from '../api'
 import responseHandler from '../../utils/responseHandler';
 
 export default async function deleteProductOfSuprimento(productAlreadyInSuprimento, setUpdate){
-      const response = api.delete('suprimentoProduto/', { productAlreadyInSuprimento })
+      const response = api.delete(`suprimentoProdutoOne/${productAlreadyInSuprimento.id}`, { productAlreadyInSuprimento })
       .then(
         (response) => {
           let status = response.status
