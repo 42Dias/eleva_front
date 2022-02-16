c77c65ed00edb499611304f1e5064a9961b8de8b
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-16 12:12:08 -0300

    refatorado o código para adicionar o filtro, agora ele atualiza o produto ao invés de adicionar novamente

--- src/utils/filterFornecedores.js
+++ src/utils/filterFornecedores.js
@@ -1,48 +1,60 @@
 export default async function filterFornecedores(produtosNoCarrinho, setFunction){
+    //variaveis para a ajuda do código  
     let containerDeObjetos = [];
     let fornecedoresNoCarrinho = [];
     
-      produtosNoCarrinho.filter(
-        async (produtoNoCarrinho) => {
-          if(!fornecedoresNoCarrinho.includes(produtoNoCarrinho.fornecedorId)){
-            fornecedoresNoCarrinho.push(produtoNoCarrinho.fornecedorId)
-          }
-        }
-      )
-      
-
-      fornecedoresNoCarrinho.map(
-        (fornecedor) =>{ 
-          const novoObj =  { "fornecedorId": fornecedor, "produtos": [] }
-          containerDeObjetos.push(novoObj)
-        }
-      )
+    // produtosNoCarrinho é um array com todos os produtos do carrinho
     
-      console.log("containerDeObjetos")
-      console.log(containerDeObjetos)
+    produtosNoCarrinho.filter(
+      async (produtoNoCarrinho) => {
 
-      console.log(produtosNoCarrinho)
-      console.log(fornecedoresNoCarrinho)
+        // caso o fornecedor já estiver na variavel fornecedoresNoCarrinho não é adicionado!
+        if(fornecedoresNoCarrinho.includes(produtoNoCarrinho.fornecedorId)) return
 
-      containerDeObjetos.map( (fornecedor, index )=>{
-        produtosNoCarrinho.filter(
-          (produtoNoCarrinho, index) => {
-            if (fornecedor.fornecedorId == produtoNoCarrinho.fornecedorId){
-              console.log("fornecedor.produtos")
-              console.log(fornecedor.produtos)
-                fornecedor.produtos.push(produtoNoCarrinho)
-                fornecedor.produtos.map(
-                  (produtoDoFornecedor) => {
-                    produtoDoFornecedor.fornecedorId = fornecedor.fornecedorId
-                  }
-                )
-            }
+        fornecedoresNoCarrinho.push(produtoNoCarrinho.fornecedorId)
+      }
+    )
+    
+    fornecedoresNoCarrinho.map(
+      (fornecedor) =>{ 
+        // cria um objeto novo para cada fornecedor!
+        const novoObj =  { "fornecedorId": fornecedor, "produtos": [] }
+        containerDeObjetos.push(novoObj)
+      }
+    )
+
+    containerDeObjetos.map( (fornecedor, index )=>{
+
+      produtosNoCarrinho.filter(
+        (produtoNoCarrinho) => {
+
+          if (!fornecedor.fornecedorId == produtoNoCarrinho.fornecedorId) return
+          
+          // caso produto já estiver no carrinho ele é atualizado
+          if(fornecedor.id == produtoNoCarrinho.id){
+            fornecedor == produtoNoCarrinho
           }
-          )
-          console.log(fornecedor, index)
-        }
+
+          else{
+
+            // adicionado o produto no fornecedor
+            fornecedor.produtos.push(produtoNoCarrinho)
+            
+            fornecedor.produtos.map(
+              (produtoDoFornecedor) => {
+                // faz a adequação do objeto
+                return produtoDoFornecedor.fornecedorId = fornecedor.fornecedorId
+              }
+            )
+          }
+          }
+
         )
-        return setFunction(containerDeObjetos)
+        // console.log(fornecedor, index)
+      }
+      )
+      // retorna a função do setState
+      return setFunction(containerDeObjetos)
       }
      
 
