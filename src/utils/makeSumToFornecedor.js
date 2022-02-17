176b486ca6637d26c274f68a31b2980878b0d271
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-17 08:03:19 -0300

    arrumada a função

--- src/utils/makeSumToFornecedor.js
+++ src/utils/makeSumToFornecedor.js
@@ -1,10 +1,10 @@
-export default function makeSumToCarrinho(fornecedor){
-    const total =  formatPrice(
-        fornecedor.produtos.reduce((sumTotal, produto) => {
+import { formatPrice } from "./format";
+
+export default function makeSumToFornecedor(fornecedor){
+    const total =  fornecedor.produtos.reduce((sumTotal, produto) => {
         sumTotal += Number(produto.produto.preco) * produto.quantidade;
         return sumTotal;
         }, 0)
-    );
     
     return total
 }
