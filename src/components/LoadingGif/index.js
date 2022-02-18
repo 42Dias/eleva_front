191760e44c685c4f75a58155d5c2c9ceab538bc5
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-18 15:18:58 -0300

    componentizado o loading

---
+++ src/components/LoadingGif/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoadingGif() {
+  return (
+    <img
+        width="40px"
+        style={{ margin: "auto" }}
+        height=""
+        src={"https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"}
+        alt="Loading"
+    />
+  )
+}
