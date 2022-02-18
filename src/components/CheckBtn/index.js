4371dc553784b9f7eec2e12b69c1a91b5b67c8f1
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-18 15:17:54 -0300

    adicionado o botão de check

---
+++ src/components/CheckBtn/index.js
@@ -0,0 +1,12 @@
+import { FiCheck } from 'react-icons/fi'
+import * as S from './styled'
+
+
+export default function CheckBtn() {
+    return (
+        <S.Check>
+            <FiCheck/>
+        </S.Check>
+    )
+  }
+  
---
+++ src/components/CheckBtn/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/macro'
+import { theme } from '../../ui/styles/theme'
+
+
+export const Check = styled.button`
+    width: 35px;
+    height: 35px;
+    border: 0;
+    border-radius: 5px;
+    background: ${theme.colors.green};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  svg {
+    color: ${theme.colors.white};
+    font-size: 18px;
+  }
+`
