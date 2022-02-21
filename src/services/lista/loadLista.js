31d900a29db1382fdd264b7e44c612408878df68
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-21 13:23:14 -0300

    agora,  apenas é puxada a lista do cliente

--- src/services/lista/loadLista.js
+++ src/services/lista/loadLista.js
@@ -1,8 +1,10 @@
 import { toast } from 'react-toastify';
-import { api } from '../api'
+import { api, id } from '../api'
 
 
 export default  async function loadLista(){
-      const listaResponse = await api.get(`lista`)
+      console.log(id)
+      const listaResponse = await api.get(`lista?filter%5Buser%5D=${id}`)
+      console.log(listaResponse)
       return listaResponse.data.rows;
     }
