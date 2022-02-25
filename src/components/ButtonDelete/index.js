e32271cd498b1eb609365a679d7ed30c1278245c
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-25 15:18:27 -0300

    arrumado o nome do componente

--- src/components/ButtonDelete/index.js
+++ src/components/ButtonDelete/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { DeleteButton } from './styled'
 import { FiTrash } from "react-icons/fi";
 
 
-export default function ButtonDelete({func}) {
+export default function ButtonDelete() {
   return (
     <>
         <DeleteButton>
