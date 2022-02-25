c2bd9812c4723b8b9321947d42ebe93c32978ae1
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-25 16:15:58 -0300

    arrumado o loading if

--- src/components/LoadingGif/index.js
+++ src/components/LoadingGif/index.js
@@ -4,10 +4,11 @@ export default function LoadingGif() {
   return (
     <img
         width="40px"
-        style={{ margin: "auto" }}
+        style={{ margin: "auto", display: 'flex',alignSelf: 'center' }}
         height=""
         src={"https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"}
         alt="Loading"
+        
     />
   )
 }
