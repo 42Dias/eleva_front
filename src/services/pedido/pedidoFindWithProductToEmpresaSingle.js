aaa9a613c0d019d619bbad52bef91bc5c4b2eeba
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-03 11:17:38 -0300

    adicionado o filter correto no fronmt

--- src/services/pedido/pedidoFindWithProductToEmpresaSingle.js
+++ src/services/pedido/pedidoFindWithProductToEmpresaSingle.js
@@ -3,10 +3,9 @@ import { api, apiWithTenantAndWithToken } from "../api";
 
 
 export default async function pedidoFindWithProductToEmpresaSingle(id){
-    return await api.get(`pedidoUser?filter=[pedidoProdutoId]=${id}`)
+    return await api.get(`pedidoUser?filter%5BpedidoProdutoId%5D=${id}`)
           .then((response) => {
-            // console.log(response)
-            let productData = response.data
+            let productData = response.data[0]
             return productData
           });
 }
