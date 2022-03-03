1121d359bba9754694c1004856df1407bf1d047e
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-03 10:29:16 -0300

    limpado o código

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { api, apiWithoutTenant, ip } from '../services/api';
+import { apiWithoutTenantAndWithToken } from '../services/api';
 
 
-export default function useInfiniteScroll(number){
+export default function useInfiniteScroll(number) {
 
 
-  const [products = [], setProducts] = useState([]);
+	const [products = [], setProducts] = useState([]);
 
-  const [loading, setLoading] = useState(false)
+	const [loading, setLoading] = useState(false)
 
-  const [hasMore, setHasMore] = useState(false) // feito para quando não há mais resultados para pesquisa
+	const [hasMore, setHasMore] = useState(false) // feito para quando não há mais resultados para pesquisa
 
-  
+
 
 	useEffect(() => {
-	setLoading(true)
+		setLoading(true)
 
-	apiWithoutTenant.get(`produto/${number}`)
-	.then(res => {
-	   setProducts(prevProducts => {
-	   		return [...new Set([...prevProducts, ...res.data])]	
-	   	   })
-			setLoading(false)
-			setHasMore(res.data.length!==0)
-			// setHasMore(true)
-	}
-	).catch(error => console.log(error))
-	}, [number] )
+		apiWithoutTenantAndWithToken.get(`produto/${number}`)
+			.then(res => {
+				setProducts(prevProducts => {
+					return [...new Set([...prevProducts, ...res.data])]
+				})
+				console.log(res.data)
+				setLoading(false)
+				setHasMore(res.data.length !== 0)
+				// setHasMore(true)
+			}
+			).catch(error => console.log(error))
+	}, [number])
 	return { loading, products, hasMore }
 
 }
